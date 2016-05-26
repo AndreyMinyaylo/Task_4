5f17ff66ee335ecd85671a23b03ea46f1a4c973e
AndreyMinyaylo <minyailoandrey@gmail.com>
2016-05-26 10:27:20 +0300

    task_4_1-4_6

--- Task_4/Task_4/Program.cs
+++ Task_4/Task_4/Program.cs
@@ -79,8 +79,8 @@ namespace Task_4
         public static int input()
         {
             Console.WriteLine("Enter number:");
-            var time = Convert.ToInt32(Console.ReadLine());
-            return time;
+            var number = Convert.ToInt32(Console.ReadLine());
+            return number;
         }
         public static void number(int someNumber)
         {
@@ -102,6 +102,125 @@ namespace Task_4
         }
     }
 
+    class consoleNumber
+    {
+        public static float input()
+        {
+            Console.WriteLine("Enter float number:");
+            var number = Convert.ToSingle(Console.ReadLine());
+            return number;
+        }
+
+        public static void numberFloat(float someNumber)
+        {
+            if ((someNumber >= 0)&&(someNumber <= 1))
+            {
+                Console.WriteLine("Number is range 0 to 1");
+            }
+        else
+                {
+                    Console.WriteLine("Number isn`t range 0 to 1");
+                }
+            Console.ReadKey();
+        }
+
+        public static void consoleOut()
+        {
+            float someNumber = input();
+            numberFloat(someNumber);
+        }
+
+    }
+
+    class twoNumbers
+    {
+        public static int input()
+        {
+            Console.WriteLine("Enter number:");
+            var number = Convert.ToInt32(Console.ReadLine());
+            return number;
+        }
+
+        public static void workWithNumber(int firstNumber, int secondNumber)
+        {
+            int max = firstNumber;
+       
+            if (secondNumber > max)
+            {
+                Console.WriteLine("Second number is biggest. Result is: " + (firstNumber + secondNumber));
+            }
+            else
+            {
+                Console.WriteLine("First number is biggest. Result is: " + (firstNumber - secondNumber));
+            }
+            Console.ReadKey();
+        }
+
+
+
+        public static void consoleOut()
+        {
+            int firstNumber = input();
+            int secondNumber = input();
+            workWithNumber(firstNumber,secondNumber);
+        }
+    }
+
+    class  task_4_6
+    {
+        public static void resultWork(int firstNumber, int secondNumber)
+        {
+            int someNumber = firstNumber + secondNumber;
+            if ((someNumber >= 11) && (someNumber <= 19))
+            {
+                Console.WriteLine("Sum number`s is: " + someNumber);
+            }
+            else
+            {
+                Console.WriteLine("Sum numbers isn`t in range of 11 to 19");
+            }
+            Console.ReadKey();
+        }
+        public static void consoleOut()
+        {
+            int firstNumber = twoNumbers.input();
+            int secondNumber = twoNumbers.input();
+            resultWork(firstNumber,secondNumber);
+
+        }
+    }
+
+   /* class task_4_7
+    {
+        public static bool resultWork(int firstNumber, int secondNumber)
+        {   
+            bool someNumber = true;
+            if ((firstNumber%secondNumber != 0) || (secondNumber%firstNumber != 0))
+            {
+                return false;
+            }
+            
+            return someNumber;
+            //Console.WriteLine("the number of divided without a remainder: " + );
+        }
+
+        public static void console(bool someNumber, int firstNumber, int secondNumber)
+        {
+            if (someNumber == true)
+            {
+                
+            }
+        }
+
+        public static void consoleOut()
+        {
+            int firstNumber = twoNumbers.input();
+            int secondNumber = twoNumbers.input();
+            resultWork(firstNumber, secondNumber);
+
+        }
+    }*/
+
 
     class writeConsole
         {
@@ -110,6 +229,9 @@ namespace Task_4
                 worktime.result();
                 biggestNumber.consoleOut();
                 twiceNumber.consoleOut();
+                consoleNumber.consoleOut();
+                twoNumbers.consoleOut();
+                task_4_6.consoleOut();
             }
         }
 
